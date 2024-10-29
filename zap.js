cf306b56ab5419d2e146fc1a5112e6a0f3be720b
Huy Pham <p.q.huy97@gmail.com>
2024-10-29 11:55:02 +0700

    log balance

--- zap.js
+++ zap.js
@@ -2,7 +2,7 @@ import { Raydium, WSOLMint } from "@raydium-io/raydium-sdk-v2";
 import { LAMPORTS_PER_SOL, VersionedTransaction, PublicKey, Connection } from "@solana/web3.js";
 import { JitoRpcConnection, searcher } from 'jito-ts';
 import { keyPairFromFile } from "./helper.js";
-import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { AccountLayout, getAssociatedTokenAddress } from "@solana/spl-token";
 
 const apiUrl = "http://localhost:8080/swapAndMint"
 const rpcUrl = "http://neproxy-crawler.core-services.svc.cluster.local:8080/neproxy/solana"
@@ -16,14 +16,17 @@ async function swapAndMint() {
     const balance = await connection.getBalance(payer);
     console.log('current account has balance: ', balance);
 
+    const poolAddress = new PublicKey("3nMFwZXwY1s1M5s8vYAHqd4wGs4iSxXE4LRoUMMYqEgF");
+    const tokenMintAddress = new PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"); // other token in pool
+
     // Step 3: Define the input parameters
     const params = {
         userAddress: payer.toBase58(),
-        poolAddress: "2AXXcN6oN9bBT5owwmTH53C7QHUXvhLeu718Kqt8rvY2",
+        poolAddress: poolAddress.toBase58(),
         swapSlippage: 0.01,
-        liquiditySlippage: 0.01,
-        tickLower: -30290,
-        tickUpper: -28170,
+        liquiditySlippage: 0.03,
+        tickLower: -18019,
+        tickUpper: -12019,
         tokenInAddress: WSOLMint.toBase58(),
         amountIn: LAMPORTS_PER_SOL * 0.01,
         protocol: "raydiumv3"
@@ -36,22 +39,22 @@ async function swapAndMint() {
         return versionTx
     })
 
-    await sendBundle(signer, txs, false);
+    await sendBundle(signer, txs, false, tokenMintAddress);
 }
 
-async function sendBundle(keypair, txs, sendTx) {
+async function sendBundle(keypair, txs, sendTx, mintAddress) {
     const blockEngineUrl = "block-engine.mainnet.frankfurt.jito.wtf"
     const conn = new JitoRpcConnection(rpcUrl);
     const client = searcher.searcherClient(blockEngineUrl);
     const _tipAccount = (await client.getTipAccounts())[0]
     const tipAccount = new PublicKey(_tipAccount);
     
-    const raydiumATA = await getAssociatedTokenAddress(new PublicKey("4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R"), keypair.publicKey);
-    console.log("raydiumATA", raydiumATA.toBase58());
+    const ata = await getAssociatedTokenAddress(mintAddress, keypair.publicKey);
+    console.log("ATA", ata.toBase58());
 
     let accounts = txs.map((tx) => {
       return {
-        addresses: [keypair.publicKey.toBase58(), raydiumATA.toBase58()],
+        addresses: [keypair.publicKey.toBase58(), ata.toBase58()],
       };
     })
   
@@ -65,8 +68,24 @@ async function sendBundle(keypair, txs, sendTx) {
     );
   
     console.log(resBundle.value.transactionResults);
-    console.log("pre state", resBundle.value.transactionResults[0]?.postExecutionAccounts);
-    console.log("post state", resBundle.value.transactionResults[0]?.postExecutionAccounts);
+
+    resBundle.value.transactionResults[0]?.preExecutionAccounts?.map((account) => {
+        if (account.data[0] == '') {
+            console.log("pre state", account.lamports)
+        } else {
+            const accountData = AccountLayout.decode(Buffer.from(account.data[0], 'base64'));
+            console.log(`pre state ${accountData.mint}   ${accountData.amount}`)
+        }
+    })
+
+    resBundle.value.transactionResults[0]?.postExecutionAccounts?.map((account) => {
+        if (account.data[0] == '') {
+            console.log("post state", account.lamports)
+        } else {
+            const accountData = AccountLayout.decode(Buffer.from(account.data[0], 'base64'));
+            console.log(`post state ${accountData.mint}   ${accountData.amount}`)
+        }
+    })
   
     if (resBundle.value.summary == "succeeded" && sendTx) {
         console.log("summary succeeded");
